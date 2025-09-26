2bcf344176fd5d76b4d2007599c36a254046b7fb
Tizian Lamatsch <21215869+Xenolphthalein@users.noreply.github.com>
2025-09-26 09:58:59 +0200

    Export progressReporter (#9)
    * Add missing export for progressReporter utility
    
    * Bump version to 1.2.2 in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./utils/checkPath.js";
 export * from "./utils/hashGenerator.js";
+export * from "./utils/progressReporter.js";
 export * from "./models/Header.js";
 export * from "./models/Metadata.js";
 export * from "./models/CLBGFile.js";
